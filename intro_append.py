464d7199a5ef1e3b7f8b19557b9bfcc825a24dc0
aeshna25 <aeshnaagg@gmail.com>
2018-10-17 16:30:10 +0530

    added new code

---
+++ intro_append.py
@@ -0,0 +1,27 @@
+numbers = [1,2,3,4,5,6,7]
+>>> numbers.append(8) # append is used to add something to the list
+>>> numbers
+[1, 2, 3, 4, 5, 6, 7, 8]
+
+>>> # append can also be used in loops
+>>> for i in range (10)
+SyntaxError: invalid syntax
+>>> for i in range (10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
+>>> 
+>>> list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> 
